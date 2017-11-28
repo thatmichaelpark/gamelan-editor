c9cdc629400b123ea4df3d6d86f6198cb72d4af0
Michael Park <thatmichaelpark@gmail.com>
2017-11-28 10:48:04 -0800

    added sample filenames

--- client/src/stores/gamelansStore.js
+++ client/src/stores/gamelansStore.js
@@ -17,110 +17,110 @@ class GamelansStore {
                 name: 'Bonang barung',
                 nHands: 2,
                 tones: [
-                    { pitch: '1̣' },
-                    { pitch: '2̣' },
-                    { pitch: '3̣' },
-                    { pitch: '4̣' },
-                    { pitch: '5̣' },
-                    { pitch: '6̣' },
-                    { pitch: '7̣' },
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '4' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '7' }
+                    { pitch: '1̣', sample: 'pelog/bonang barung/bonang barung p lo1.mp3' },
+                    { pitch: '2̣', sample: 'pelog/bonang barung/bonang barung p lo2.mp3' },
+                    { pitch: '3̣', sample: 'pelog/bonang barung/bonang barung p lo3.mp3' },
+                    { pitch: '4̣', sample: 'pelog/bonang barung/bonang barung p lo4.mp3' },
+                    { pitch: '5̣', sample: 'pelog/bonang barung/bonang barung p lo5.mp3' },
+                    { pitch: '6̣', sample: 'pelog/bonang barung/bonang barung p lo6.mp3' },
+                    { pitch: '7̣', sample: 'pelog/bonang barung/bonang barung p lo7.mp3' },
+                    { pitch: '1', sample: 'pelog/bonang barung/bonang barung p1.mp3' },
+                    { pitch: '2', sample: 'pelog/bonang barung/bonang barung p2.mp3' },
+                    { pitch: '3', sample: 'pelog/bonang barung/bonang barung p3.mp3' },
+                    { pitch: '4', sample: 'pelog/bonang barung/bonang barung p4.mp3' },
+                    { pitch: '5', sample: 'pelog/bonang barung/bonang barung p5.mp3' },
+                    { pitch: '6', sample: 'pelog/bonang barung/bonang barung p6.mp3' },
+                    { pitch: '7', sample: 'pelog/bonang barung/bonang barung p7.mp3' }
                 ]
             },
             {
                 name: 'Bonang panerus',
                 nHands: 2,
                 tones: [
-                    { pitch: '1̣' },
-                    { pitch: '2̣' },
-                    { pitch: '3̣' },
-                    { pitch: '4̣' },
-                    { pitch: '5̣' },
-                    { pitch: '6̣' },
-                    { pitch: '7̣' },
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '4' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '7' }
+                    { pitch: '1̣', sample: 'pelog/bonang panerus/bonang panerus p lo1.mp3' },
+                    { pitch: '2̣', sample: 'pelog/bonang panerus/bonang panerus p lo2.mp3' },
+                    { pitch: '3̣', sample: 'pelog/bonang panerus/bonang panerus p lo3.mp3' },
+                    { pitch: '4̣', sample: 'pelog/bonang panerus/bonang panerus p lo4.mp3' },
+                    { pitch: '5̣', sample: 'pelog/bonang panerus/bonang panerus p lo5.mp3' },
+                    { pitch: '6̣', sample: 'pelog/bonang panerus/bonang panerus p lo6.mp3' },
+                    { pitch: '7̣', sample: 'pelog/bonang panerus/bonang panerus p lo7.mp3' },
+                    { pitch: '1', sample: 'pelog/bonang panerus/bonang panerus p1.mp3' },
+                    { pitch: '2', sample: 'pelog/bonang panerus/bonang panerus p2.mp3' },
+                    { pitch: '3', sample: 'pelog/bonang panerus/bonang panerus p3.mp3' },
+                    { pitch: '4', sample: 'pelog/bonang panerus/bonang panerus p4.mp3' },
+                    { pitch: '5', sample: 'pelog/bonang panerus/bonang panerus p5.mp3' },
+                    { pitch: '6', sample: 'pelog/bonang panerus/bonang panerus p6.mp3' },
+                    { pitch: '7', sample: 'pelog/bonang panerus/bonang panerus p7.mp3' }
                 ]
             },
             {
                 name: 'Gong ageng',
                 nHands: 1,
                 tones: [
-                    { pitch: 'G' }
+                    { pitch: 'G', sample: 'pelog/gong/gong pelog.mp3' }
                 ]
             },
             {
                 name: 'Gong suwuk',
                 nHands: 1,
                 tones: [
-                    { pitch: 'S' }
+                    { pitch: 'S', sample: 'pelog/gong/gong suwuk p2.mp3' }
                 ]
             },
             {
                 name: 'Nong',
                 nHands: 1,
                 tones: [
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '7' }
+                    { pitch: '1', sample: 'pelog/nong/long p1.mp3' },
+                    { pitch: '2', sample: 'pelog/nong/long p2.mp3' },
+                    { pitch: '3', sample: 'pelog/nong/long p3.mp3' },
+                    { pitch: '5', sample: 'pelog/nong/long p5.mp3' },
+                    { pitch: '6', sample: 'pelog/nong/long p6.mp3' },
+                    { pitch: '7', sample: 'pelog/nong/long p7.mp3' }
                 ]
             },
             {
                 name: 'Peking',
                 nHands: 1,
                 tones: [
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '4' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '7' }
+                    { pitch: '1', sample: 'pelog/peking/peking p1.mp3' },
+                    { pitch: '2', sample: 'pelog/peking/peking p2.mp3' },
+                    { pitch: '3', sample: 'pelog/peking/peking p3.mp3' },
+                    { pitch: '4', sample: 'pelog/peking/peking p4.mp3' },
+                    { pitch: '5', sample: 'pelog/peking/peking p5.mp3' },
+                    { pitch: '6', sample: 'pelog/peking/peking p6.mp3' },
+                    { pitch: '7', sample: 'pelog/peking/peking p7.mp3' }
                 ]
             },
             {
                 name: 'Pul',
                 nHands: 1,
                 tones: [
-                    { pitch: '1' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '7' }
+                    { pitch: '1', sample: 'pelog/pul/pul p1.mp3' },
+                    { pitch: '3', sample: 'pelog/pul/pul p3.mp3' },
+                    { pitch: '5', sample: 'pelog/pul/pul p5.mp3' },
+                    { pitch: '6', sample: 'pelog/pul/pul p6.mp3' },
+                    { pitch: '7', sample: 'pelog/pul/pul p7.mp3' }
                 ]
             },
             {
                 name: 'Saron',
                 nHands: 1,
                 tones: [
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '4' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '7' }
+                    { pitch: '1', sample: 'pelog/saron/saron p1.mp3' },
+                    { pitch: '2', sample: 'pelog/saron/saron p2.mp3' },
+                    { pitch: '3', sample: 'pelog/saron/saron p3.mp3' },
+                    { pitch: '4', sample: 'pelog/saron/saron p4.mp3' },
+                    { pitch: '5', sample: 'pelog/saron/saron p5.mp3' },
+                    { pitch: '6', sample: 'pelog/saron/saron p6.mp3' },
+                    { pitch: '7', sample: 'pelog/saron/saron p7.mp3' }
                 ]
             },
             {
                 name: 'Tuk',
                 nHands: 1,
                 tones: [
-                    { pitch: '+' }
+                    { pitch: '+', sample: 'pelog/tuk pelog.mp3' }
                 ]
             }
         ]),
@@ -129,106 +129,106 @@ class GamelansStore {
                 name: 'Bonang barung',
                 nHands: 2,
                 tones: [
-                    { pitch: '1̣' },
-                    { pitch: '2̣' },
-                    { pitch: '3̣' },
-                    { pitch: '5̣' },
-                    { pitch: '6̣' },
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '1̇' },
-                    { pitch: '2̇' }
+                    { pitch: '1̣', sample: 'slendro/bonang barung/bonang barung s lo1.mp3' },
+                    { pitch: '2̣', sample: 'slendro/bonang barung/bonang barung s lo2.mp3' },
+                    { pitch: '3̣', sample: 'slendro/bonang barung/bonang barung s lo3.mp3' },
+                    { pitch: '5̣', sample: 'slendro/bonang barung/bonang barung s lo5.mp3' },
+                    { pitch: '6̣', sample: 'slendro/bonang barung/bonang barung s lo6.mp3' },
+                    { pitch: '1', sample: 'slendro/bonang barung/bonang barung s1.mp3' },
+                    { pitch: '2', sample: 'slendro/bonang barung/bonang barung s2.mp3' },
+                    { pitch: '3', sample: 'slendro/bonang barung/bonang barung s3.mp3' },
+                    { pitch: '5', sample: 'slendro/bonang barung/bonang barung s5.mp3' },
+                    { pitch: '6', sample: 'slendro/bonang barung/bonang barung s6.mp3' },
+                    { pitch: '1̇', sample: 'slendro/bonang barung/bonang barung s hi1.mp3' },
+                    { pitch: '2̇', sample: 'slendro/bonang barung/bonang barung s hi2.mp3' }
                 ]
             },
             {
                 name: 'Bonang panerus',
                 nHands: 2,
                 tones: [
-                    { pitch: '1̣' },
-                    { pitch: '2̣' },
-                    { pitch: '3̣' },
-                    { pitch: '5̣' },
-                    { pitch: '6̣' },
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '1̇' },
-                    { pitch: '2̇' }
+                    { pitch: '1̣', sample: 'slendro/bonang panerus/bonang panerus s lo1.mp3' },
+                    { pitch: '2̣', sample: 'slendro/bonang panerus/bonang panerus s lo2.mp3' },
+                    { pitch: '3̣', sample: 'slendro/bonang panerus/bonang panerus s lo3.mp3' },
+                    { pitch: '5̣', sample: 'slendro/bonang panerus/bonang panerus s lo5.mp3' },
+                    { pitch: '6̣', sample: 'slendro/bonang panerus/bonang panerus s lo6.mp3' },
+                    { pitch: '1', sample: 'slendro/bonang panerus/bonang panerus s1.mp3' },
+                    { pitch: '2', sample: 'slendro/bonang panerus/bonang panerus s2.mp3' },
+                    { pitch: '3', sample: 'slendro/bonang panerus/bonang panerus s3.mp3' },
+                    { pitch: '5', sample: 'slendro/bonang panerus/bonang panerus s5.mp3' },
+                    { pitch: '6', sample: 'slendro/bonang panerus/bonang panerus s6.mp3' },
+                    { pitch: '1̇', sample: 'slendro/bonang panerus/bonang panerus s hi1.mp3' },
+                    { pitch: '2̇', sample: 'slendro/bonang panerus/bonang panerus s hi2.mp3' }
                 ]
             },
             {
                 name: 'Gong ageng',
                 nHands: 1,
                 tones: [
-                    { pitch: 'G' }
+                    { pitch: 'G', sample: 'slendro/gong/gong ageng slendro.mp3' }
                 ]
             },
             {
                 name: 'Gong suwuk',
                 nHands: 1,
                 tones: [
-                    { pitch: 'S' }
+                    { pitch: 'S', sample: 'slendro/gong/gong suwuk s1.mp3' }
                 ]
             },
             {
                 name: 'Nong',
                 nHands: 1,
                 tones: [
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '1̇' }
+                    { pitch: '2', sample: 'slendro/nong/nong s2.mp3' },
+                    { pitch: '3', sample: 'slendro/nong/nong s3.mp3' },
+                    { pitch: '5', sample: 'slendro/nong/nong s5.mp3' },
+                    { pitch: '6', sample: 'slendro/nong/nong s6.mp3' },
+                    { pitch: '1̇', sample: 'slendro/nong/nong s1̇.mp3' }
                 ]
             },
             {
                 name: 'Peking',
                 nHands: 1,
                 tones: [
-                    { pitch: '6̣' },
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '1̇' }
+                    { pitch: '6̣', sample: 'slendro/peking/peking s lo6.mp3' },
+                    { pitch: '1', sample: 'slendro/peking/peking s1.mp3' },
+                    { pitch: '2', sample: 'slendro/peking/peking s2.mp3' },
+                    { pitch: '3', sample: 'slendro/peking/peking s3.mp3' },
+                    { pitch: '5', sample: 'slendro/peking/peking s5.mp3' },
+                    { pitch: '6', sample: 'slendro/peking/peking s6.mp3' },
+                    { pitch: '1̇', sample: 'slendro/peking/peking s hi1.mp3' }
                 ]
             },
             {
                 name: 'Pul',
                 nHands: 1,
                 tones: [
-                    { pitch: '1̇' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' }
+                    { pitch: '3', sample: 'slendro/pul/pul s3.mp3' },
+                    { pitch: '5', sample: 'slendro/pul/pul s5.mp3' },
+                    { pitch: '6', sample: 'slendro/pul/pul s6.mp3' },
+                    { pitch: '1̇', sample: 'slendro/pul/pul s1̇.mp3' }
                 ]
             },
             {
                 name: 'Saron',
                 nHands: 1,
                 tones: [
-                    { pitch: '6̣' },
-                    { pitch: '1' },
-                    { pitch: '2' },
-                    { pitch: '3' },
-                    { pitch: '5' },
-                    { pitch: '6' },
-                    { pitch: '1̇' },
-                    { pitch: '2̇' },
-                    { pitch: '3̇' }
+                    { pitch: '6̣', sample: 'slendro/saron/saron s lo6.mp3' },
+                    { pitch: '1', sample: 'slendro/saron/saron s1.mp3' },
+                    { pitch: '2', sample: 'slendro/saron/saron s2.mp3' },
+                    { pitch: '3', sample: 'slendro/saron/saron s3.mp3' },
+                    { pitch: '5', sample: 'slendro/saron/saron s5.mp3' },
+                    { pitch: '6', sample: 'slendro/saron/saron s6.mp3' },
+                    { pitch: '1̇', sample: 'slendro/saron/saron s hi1.mp3' },
+                    { pitch: '2̇', sample: 'slendro/saron/saron s hi2.mp3' },
+                    { pitch: '3̇', sample: 'slendro/saron/saron s hi3.mp3' }
                 ]
             },
             {
                 name: 'Tuk',
                 nHands: 1,
                 tones: [
-                    { pitch: '+' }
+                    { pitch: '+', sample: 'slendro/tuk slendro.mp3' }
                 ]
             }
         ])
