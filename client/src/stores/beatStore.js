d3b50df7504f0905adcb68beaab69e9b0e7c8a8a
Michael Park <thatmichaelpark@gmail.com>
2017-12-21 01:09:15 -0800

    cleaning out dead files

--- client/src/stores/beatStore.js
+++ client/src/stores/beatStore.js
@@ -1,6 +1,21 @@
 import { observable } from 'mobx';
 import { currentPiece } from './piecesStore';
-import interpolator from '../interpolator';
+
+function interpolator(pointsList, t) {
+    // pointsList: [{t0, f0}, {t1, f1}...] describing a piecewise-linear curve
+    // returns f on curve at given t
+
+    for (let i = 0; i < pointsList.length - 1; ++i) {
+        const t0 = pointsList[i].t;
+        const f0 = pointsList[i].f;
+        const t1 = pointsList[i + 1].t;
+        const f1 = pointsList[i + 1].f;
+        if (t0 <= t && t <= t1) {
+            const f = f0 + (f1 - f0) * (t - t0) / (t1 - t0);
+            return f;
+        }
+    }
+}
 
 class BeatStore {
     @observable realBeat; // floating-point beat [0..nBeats)
