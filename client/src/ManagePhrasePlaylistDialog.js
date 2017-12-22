4582664ed0942c16e91f020fc98689a5cde08d34
Michael Park <thatmichaelpark@gmail.com>
2017-12-21 22:49:22 -0800

    react-flip-move on phrasePlaylist dialog

--- client/src/ManagePhrasePlaylistDialog.js
+++ client/src/ManagePhrasePlaylistDialog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { currentPiece } from './stores/piecesStore';
 import { observer } from 'mobx-react';
+import FlipMove from 'react-flip-move';
 
 @observer
 class ManagePhraseListDialog extends React.Component {
@@ -15,6 +16,7 @@ class ManagePhraseListDialog extends React.Component {
             this.setState({
                 selectedPhraseId: currentPiece.phraseInfos[0].id
             });
+            this.keys = currentPiece.phrasePlaylist.map((p, i) => i);
             this.savedPhrasePlaylist = JSON.stringify(currentPiece.phrasePlaylist);
         }
     }
@@ -26,6 +28,8 @@ class ManagePhraseListDialog extends React.Component {
     handleClick = (e) => {
         if (e.target.name === 'add') {
             currentPiece.phrasePlaylist.push(this.state.selectedPhraseId);
+            const newKey = this.keys.reduce((maxKey, key) => Math.max(maxKey, key), -1) + 1;
+            this.keys.push(newKey);
         }
         else if (e.target.name === 'ok') {
             this.props.onManagePhrasePlaylist();
@@ -43,6 +47,9 @@ class ManagePhraseListDialog extends React.Component {
             const movingPhraseId = currentPiece.phrasePlaylist[i];
             currentPiece.phrasePlaylist.splice(i, 1);                   // remove ith element
             currentPiece.phrasePlaylist.splice(i - 1, 0, movingPhraseId);   // insert moved element
+            const temp = this.keys[i];
+            this.keys[i] = this.keys[i - 1];
+            this.keys[i - 1] = temp;
         }
         const moveDown = (i) => {
             if (i === currentPiece.phrasePlaylist.length - 1) {
@@ -51,9 +58,13 @@ class ManagePhraseListDialog extends React.Component {
             const movingPhraseId = currentPiece.phrasePlaylist[i];
             currentPiece.phrasePlaylist.splice(i, 1);                   // remove ith element
             currentPiece.phrasePlaylist.splice(i + 1, 0, movingPhraseId);   // insert moved element
+            const temp = this.keys[i];
+            this.keys[i] = this.keys[i + 1];
+            this.keys[i + 1] = temp;
         }
         const deleet = (i) => {
             currentPiece.phrasePlaylist.splice(i, 1);                   // remove ith element
+            this.keys.splice(i, 1);
         }
 
         return this.props.isVisible && (
@@ -61,14 +72,16 @@ class ManagePhraseListDialog extends React.Component {
                 <div className="dialog dialog-large">
                     <h1>Manage Parts</h1>
                     <div className="dialog-contents">
-                        {currentPiece.phrasePlaylist.map((phraseId, i) =>
-                            <div key={i}>
-                                <button onClick={() => moveUp(i)}>▲</button>
-                                <button onClick={() => moveDown(i)}>▼</button>
-                                <button onClick={() => deleet(i)}>❌</button> {/* ×❌❎*/}
-                                {currentPiece.phraseInfos.find(p => p.id === phraseId).name}
-                            </div>
-                        )}
+                        <FlipMove>
+                            {currentPiece.phrasePlaylist.map((phraseId, i) =>
+                                <div key={this.keys[i]}>
+                                    <button onClick={() => moveUp(i)}>▲</button>
+                                    <button onClick={() => moveDown(i)}>▼</button>
+                                    <button onClick={() => deleet(i)}>❌</button> {/* ×❌❎*/}
+                                    {currentPiece.phraseInfos.find(p => p.id === phraseId).name}
+                                </div>
+                            )}
+                        </FlipMove>
                     </div>
                     <select onChange={this.handleSelect} value={this.state.selectedPhraseId}>
                         {currentPiece.phraseInfos.map((phraseInfo, i) =>
